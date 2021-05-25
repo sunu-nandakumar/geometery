b05c18711e601438d2f034d94ead5b7c7f6869fa
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-05-25 09:02:54 +0530

    adding the equality part to the code

--- geometery.java
+++ geometery.java
@@ -15,6 +15,23 @@ int ytwo = myObj.nextInt();
 
 double length = Math.sqrt(Math.pow((xtwo - xone), 2 ) + Math.pow((ytwo - yone) , 2));
 System.out.println(" the length of the line is " + length);
+
+
+System.out.println( " Enter aone , atwo , bone and btwo : ");
+
+int aone = myObj.nextInt();
+int atwo = myObj.nextInt();
+int bone = myObj.nextInt();
+int btwo = myObj.nextInt();
+
+double length1 = Math.sqrt(Math.pow((atwo - aone), 2 ) + Math.pow((btwo - bone) , 2));
+System.out.println(" the length of the line is " + length);
+
+if (length == length1)
+{
+	System.out.println(" the length of both lines is equal  " + length  + length1);
+}
+
 }
 
 }
